63f4ab1d2052927b6d6f2d5a0dfc5c1fbdde6380
Rodrigo Moraes <rodlmoraes@hotmail.com>
2020-03-23 22:58:26 -0300

    adiciona as rotas para as funções criadas nos controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,15 @@
 const { Router } = require('express')
+const KarController = require('./controllers/KarController')
+const SearchController = require('./controllers/SearchController')
 
 const routes = Router()
+
+routes.get('/devs', KarController.index)
+routes.get('/devs/:id', KarController.show)
+routes.post('/devs', KarController.store)
+routes.put('/devs/:id', KarController.update)
+routes.delete('/devs/:id', KarController.destroy)
+
+routes.get('/search', SearchController.index)
 
 module.exports = routes
