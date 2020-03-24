187333642a1c5ef3e67756e9d60934cc67095a8e
Rodrigo Moraes <rodlmoraes@hotmail.com>
2020-03-24 11:59:18 -0300

    quick correction

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,11 +4,11 @@ const SearchController = require('./controllers/SearchController')
 
 const routes = Router()
 
-routes.get('/devs', KarController.index)
-routes.get('/devs/:id', KarController.show)
-routes.post('/devs', KarController.store)
-routes.put('/devs/:id', KarController.update)
-routes.delete('/devs/:id', KarController.destroy)
+routes.get('/kars', KarController.index)
+routes.get('/kars/:id', KarController.show)
+routes.post('/kars', KarController.store)
+routes.put('/kars/:id', KarController.update)
+routes.delete('/kars/:id', KarController.destroy)
 
 routes.get('/search', SearchController.index)
 
